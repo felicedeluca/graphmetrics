e4de4820ffbcf285da2f4cfd76f4c4e36c984fec
felicedeluca <felicedeluca@me.com>
2019-05-03 11:33:42 -0700

    updated sh

--- compute_metrics.sh
+++ compute_metrics.sh
@@ -3,5 +3,5 @@
 for file in "/Users/felicedeluca/Developer/UofA/gdcontest/dags_med/drawings/dotalgorithm"/*.dot
 do
   echo "$file"
-  python3 metricscomputer.py "$file"  "upflow,st" "/Users/felicedeluca/Developer/UofA/gdcontest/dags_med/drawings/dotalgorithm/measures.txt"
+  python3 metricscomputer.py "$file"  "/Users/felicedeluca/Developer/UofA/gdcontest/dags_med/drawings/dotalgorithm/measures.txt" "upflow,st"
 done
