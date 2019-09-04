063a88b3a36c11d02dfe91b3dbac9b68e55cd550
felicedeluca <felicedeluca@me.com>
2019-09-04 09:41:32 -0700

    minor issue fixed

--- graphconverter/dot2gml.py
+++ graphconverter/dot2gml.py
@@ -25,14 +25,35 @@ fontSize = nx.get_node_attributes(G, "fontsize")
 fontname = nx.get_node_attributes(G, 'fontname')
 
 for v in nx.nodes(G):
-    graphics = {'x' : float(pos[v].split(",")[0]),
-                'y' : float(pos[v].split(",")[1]),
-                'label' : labels[v],
+
+    label = v
+    if v in labels.keys():
+        label = labels[v]
+
+    fs = 1
+    if v in fontSize.keys():
+        fs = fontSize[v]
+
+    fn = 1
+    if v in fontSize.keys():
+        fn = fontname[v]
+
+    x_pos = 0
+    y_pos = 0
+
+    if v in pos:
+        x_pos = float(pos[v].split(",")[0])
+        y_pos = float(pos[v].split(",")[1])
+
+
+    graphics = {'x' : x_pos,
+                'y' : y_pos,
+                'label' : label,
                 'type': 'rectangle',
                 'raisedBorder':'0'}
 
 
-    lblgraphics = {'text' : labels[v], 'fontSize':fontSize[v], 'fontName':fontname[v]}
+    lblgraphics = {'text' : label, 'fontSize':fs, 'fontName':fn}
     nx.set_node_attributes(G, {v:graphics}, "graphics")
     # nx.set_node_attributes(G, {v:labels[v]}, "label")
     # nx.set_node_attributes(G, {v:labels[v]}, "text")
